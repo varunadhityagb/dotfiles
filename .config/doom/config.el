906a82736a31e32cc393f12a3f93731de52cec86
varunadhityagb <varunadhitya.balaji@gmail.com>
2025-04-06 16:46:50 +0530

    missed a file

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,19 +1,50 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-(setq doom-theme 'doom-monokai-octagon)
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16 :weight 'regular))
+(setq doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 14 :weight 'regular))
+
+(setq doom-theme 'doom-monokai-ristretto)
+(add-to-list 'default-frame-alist '(alpha-background . 75))
 
 (setq display-line-numbers-type 'relative)
 
 (setq org-directory "~/org/")
-(setq org-latex-listings t)
 (setq org-latex-src-block-backend 'listings)
 
 (setq conda-anaconda-home (getenv "ANACONDA_HOME")
       conda-home-candidates
       (list "~/.anaconda"))
+
+;; Set pdflatex as the default compilation method
+(setq +latex-default-compilation-method 'pdflatex)
+
+;; Ensure SyncTeX is enabled and pdflatex is used
+(after! tex
+  (setq TeX-command-default "pdflatex")
+  (setq TeX-parse-self t)
+  (setq TeX-auto-save t)
+  (setq-default TeX-master t)
+
+  (add-to-list 'TeX-command-list
+               '("PdfLaTeX" "pdflatex -synctex=1 %s" TeX-run-TeX nil t))
+
+  ;; Set the default LaTeX engine for pdflatex
+  (setq TeX-command-list
+        (append TeX-command-list
+                '(("PdfLaTeX" "pdflatex -synctex=1 %s" TeX-run-TeX nil t)))))
+
+(setq TeX-PDF-mode t)  ;; Ensure PDF output is generated
+
+;; Ensure pdflatex is in the path if necessary
+(setq exec-path (append exec-path '("/usr/local/texlive/2024/bin/x86_64")))
+
+;; PDF tools setup for SyncTeX support
+(after! pdf-tools
+  (setq pdf-view-resize-factor 1.1)  ;; Optional, tweak the zoom level
+  (add-to-list 'revert-without-query ".pdf"))
+
+;;(define-key pdf-view-mode-map (kbd "M-<mouse-1>") #'pdf-view-sync-backward)
+;;(define-key pdf-view-mode-map (kbd "M-<mouse-3>") #'pdf-view-sync-forward)
 
 (use-package! pdf-tools
   :defer t
@@ -31,24 +62,14 @@
   (add-hook 'pdf-view-mode-hook
             (lambda ()
               (when (eq major-mode 'pdf-view-mode)
-                (setq pdf-sync-embed-pdf t)))))
-
-
-(add-hook 'pdf-view-mode-hook
-          (lambda ()
-            (display-line-numbers-mode -1)))
-
-;; Enable LSP
-(lsp)
+                (setq pdf-sync-embed-pdf t))))
+  ;; Bind keys for forward and backward SyncTeX search
+  (define-key pdf-view-mode-map (kbd "M-<mouse-1>") #'pdf-view-sync-backward)
+  (define-key pdf-view-mode-map (kbd "M-<mouse-3>") #'pdf-view-sync-forward))
 
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((octave . t)))
-
-;; Comment line
-(map! :leader "z" #'comment-line)
-
-(setq +latex-viewers '(zathura))
 
 (setq confirm-kill-emacs nil)
 
@@ -62,14 +83,6 @@
   (set-face-attribute 'org-level-3 nil :height 1.1 :weight 'normal)
   (set-face-attribute 'org-document-title nil :height 1.5 :weight 'bold))
 
-(after! tex
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
-  (setq-default TeX-master t)
-  (setq TeX-command-default "LatexMk")
-  (setq latexmk-options (append latexmk-options '("-synctex=1")))
-  )
-
 (use-package! latex-preview-pane
   :after latex
   :commands latex-preview-pane-mode
@@ -78,12 +91,11 @@
   (setq latex-preview-pane-update-delay 0.1)
   :config
   (setq latex-preview-pane-pdf-view-command "zathura")
-
+  (setq latex-preview-pane-latex-command "pdflatex -synctex=1 -interaction=nonstopmode %f")
   ;; Enable the preview pane
   (latex-preview-pane-enable))
 
 (after! org (setq org-babel-default-header-args:java '((:dir . nil) (:results . "value")))) (org-babel-do-load-languages 'org-babel-load-languages '((java . t)))
-
 
 ;; Better auto-save configuration
 (add-hook! 'latex-mode-hook
@@ -95,10 +107,6 @@
     (setq auto-save-timeout 1)
     (setq auto-save-interval 1)))
 
-(setq exec-path (append exec-path '("/usr/local/textlive/2024/bin/x86_64/pdflatex")) )
-
-(setq copilot-mode nil)
-
 (defun insert-setupfile()
   "Inserts setupfile"
   (interactive)
@@ -108,8 +116,6 @@
       :desc "Open vterm" "o t" #'vterm)
 
 (map! :leader
-      :localleader
-      "p" #'latex-preview-pane-mode)
-
-(map! :leader
       "b a b i" #'insert-setupfile )
+
+
