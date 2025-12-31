4848fcdcb9e30def0f076c3565bb1b9cf7412a26
varunadhityagb <varunadhitya.balaji@gmail.com>
2025-12-31 11:17:49 +0530

    made a hypr.org to manage evrything in one file

--- .config/hypr/scripts/hdmiInserted.sh
+++ .config/hypr/scripts/hdmiInserted.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/bin/hyprctl keyword monitor HDMI-A-1, prefered, 0x0, 1
-/usr/bin/hyprctl keyword monitor eDP-1, 1600x900,auto,1
+/usr/bin/hyprctl keyword monitor eDP-1, 1920x1080,auto,1
--- .config/hypr/scripts/hdmiRemoved.sh
+++ .config/hypr/scripts/hdmiRemoved.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/bin/hyprctl keyword monitor HDMI-A-1,disable
-/usr/bin/hyprctl keyword monitor eDP-1,1600x900,auto,1
+/usr/bin/hyprctl keyword monitor eDP-1,1920x1080,auto,1
